cc93c3033b999c1f597ed41ed70e09608c16f900
Alexey Belkov <knots@ya.ru>
2014-09-02 13:20:05 +0400

    Refactor user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe User do
-	before do
-		@user = User.new(first_name: 'John', last_name: 'Doe', email: 'jd@ex.com',
-			               password: 'foobar', password_confirmation: 'foobar')
-	end
 
-	subject { @user }
+	let(:user) { FactoryGirl.create(:user) }
+	subject { user }
 
 	it { should respond_to(:first_name) }
 	it { should respond_to(:last_name) }
@@ -19,57 +16,57 @@ describe User do
 
 	describe "first name" do
 		describe "not present" do
-			before { @user.first_name = " " }
+			before { user.first_name = " " }
 			it { should_not be_valid }
 		end
 
 		describe "too long" do
-			before { @user.first_name = "a" * 51 }
+			before { user.first_name = "a" * 51 }
 			it { should_not be_valid }
 		end
 
 		describe "in lowercase" do
 			before do
-				@user.first_name = 'john'
-				@user.save
+				user.first_name = 'alexey'
+				user.save
 			end
-			its(:first_name) { should eq 'John' }
+			its(:first_name) { should eq 'Alexey' }
 		end
 	end
 
 	describe "last name" do
 		describe "not present" do
-			before { @user.last_name = " " }
+			before { user.last_name = " " }
 			it { should_not be_valid }
 		end
 
 		describe "too long" do
-			before { @user.last_name = "a" * 51 }
+			before { user.last_name = "a" * 51 }
 			it { should_not be_valid }
 		end
 
 		describe "in lowercase" do
 			before do
-				@user.last_name = 'doe'
-				@user.save
+				user.last_name = 'belkov'
+				user.save
 			end
-			its(:last_name) { should eq 'Doe' }
+			its(:last_name) { should eq 'Belkov' }
 		end
 	end
 
 	describe "full name" do
-		before { @user.save }
-		its(:full_name) { should eq "John Doe" }
+		before { user.save }
+		its(:full_name) { should eq "Alexey Belkov" }
 	end
 
 	describe "email" do
 		describe "not present" do
-			before { @user.email = " " }
+			before { user.email = " " }
 			it { should_not be_valid }
 		end
 
 		describe "too long" do
-			before { @user.email = "a@b.com" * 10 }
+			before { user.email = "a" * 45 + "@b.com" }
 			it { should_not be_valid }
 		end
 
@@ -78,8 +75,8 @@ describe User do
 				invalid = %w[user@foo,com foo@bar..com user_at_foo.org
 					           example.user@foo.foo@bar_baz.com foo@bar+baz.com]
 				invalid.each do |email|
-					@user.email = email
-					expect(@user).not_to be_valid
+					user.email = email
+					expect(user).not_to be_valid
 				end
 			end
 		end
@@ -88,55 +85,54 @@ describe User do
 			it 'should be valid' do
 				valid = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
 				valid.each do |email|
-					@user.email = email
-					expect(@user).to be_valid
+					user.email = email
+					expect(user).to be_valid
 				end
 			end
 		end
 
 		describe "already taken" do
-			before do
-				user_with_same_email = @user.dup
-				user_with_same_email.email = @user.email.upcase
-				user_with_same_email.save
+			it "should raise" do
+				expect do
+					user_with_same_email = FactoryGirl.create(:user)
+					user_with_same_email.email = user.email.upcase
+				end.to raise_error(ActiveRecord::RecordInvalid)
 			end
-			it { should_not be_valid }
 		end
 	end
 
 	describe "password" do
 
 		describe "not present" do
-			before do
-				@user = User.new(first_name: 'John', last_name: 'Doe',
-					               email: 'jd@ex.com', password: ' ',
-					               password_confirmation: ' ')
+			it "should raise" do
+			  expect do
+			  	FactoryGirl.create(:user, password: ' ', password_confirmation: ' ')
+			  end.to raise_error(ActiveRecord::RecordInvalid)
 			end
-			it { should_not be_valid }
 		end
 
 		describe "doesn't match confirmation" do
-			before { @user.password = 'mismatch' }
+			before { user.password = 'mismatch' }
 			it { should_not be_valid }
 		end
 
 		describe "too short" do
-			before { @user.password = @user.password_confirmation = 'a' * 5 }
+			before { user.password = user.password_confirmation = 'a' * 5 }
 			it { should_not be_valid }
 		end
 
 		describe "too long" do
-			before { @user.password = @user.password_confirmation = 'a' * 33 }
+			before { user.password = user.password_confirmation = 'a' * 33 }
 			it { should_not be_valid }
 		end
 	end
 
 	describe "return value of authenticate method" do
-		before { @user.save }
-		let(:found_user) { User.find_by(email: @user.email) }
+		before { user.save }
+		let(:found_user) { User.find_by(email: user.email) }
 
 		describe "with valid password" do
-			it { should eq found_user.authenticate(@user.password) }
+			it { should eq found_user.authenticate(user.password) }
 		end
 
 		describe "with invalid password" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,6 +10,7 @@ describe "User pages" do
 	end
 
 	describe "profile page" do
+		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
 		it { should have_title(user.full_name) }
 		it { should have_content(user.full_name) }
