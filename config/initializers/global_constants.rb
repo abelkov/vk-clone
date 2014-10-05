eb9760d254150970252d372ebbf419fe5ec5bdb4
Alexey Belkov <knots@ya.ru>
2014-10-05 20:45:12 +0400

    Make "- None selected -" not appear on the profile page

--- config/initializers/global_constants.rb
+++ config/initializers/global_constants.rb
@@ -1,3 +1,4 @@
 SEX = ["Male", "Female", "- None selected -"]
 RELATIONSHIP = ["Single", "In a relationship", "Engaged", "Married",
 	"In love", "It's complicated", "Actively searching", "- None selected -"]
+HIDDEN = ["- None selected -", "", nil]
